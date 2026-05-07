6ff9e5f59cdfd1a061c678c513d5418e18911287
Kathlynleticia <kathlynsantosleticia@gmail.com>
2026-05-07 00:48:54 -0300

    atualizando alterções simples sem grandes impactos

--- src/main/java/projeto/literatura/service/ConverteDados.java
+++ src/main/java/projeto/literatura/service/ConverteDados.java
@@ -10,13 +10,13 @@ public class ConverteDados implements IconverteDados{
     @Override
     public <T> T pegarDados(String json, Class<T> classe) {
         if (json == null || json.trim().isEmpty()) {
-            throw new RuntimeException("❗ O JSON retornado está vazio. Verifique a URL ou a resposta da API.");
+            throw new RuntimeException("O JSON retornado está vazio. Verifique a URL ou a resposta da API.");
         }
 
         try {
             return mapper.readValue(json, classe);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException("❌ Erro ao converter JSON: " + e.getMessage(), e);
+            throw new RuntimeException("Erro ao converter JSON: " + e.getMessage(), e);
         }
     }
 }
